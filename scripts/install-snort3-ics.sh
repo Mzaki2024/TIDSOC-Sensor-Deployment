048ed9d6c018c28b4bec9dde3eca7b7a9b800880
Mzaki2024 <mohammedzaki1904@gmail.com>
2025-05-25 18:36:51 -0400

    Update install-snort3-ics.sh

--- scripts/install-snort3-ics.sh
+++ scripts/install-snort3-ics.sh
@@ -46,7 +46,6 @@ cd ragel-6.10 && ./configure && make && sudo make install
 cd $SRC_DIR
 
 # Boost 1.77 (source only)
-cd ~/snort_src
 wget -q https://boostorg.jfrog.io/artifactory/main/release/1.77.0/source/boost_1_77_0.tar.bz2
 tar -xvjf boost_1_77_0.tar.bz2
 
